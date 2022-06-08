9edb5e2f73a2f920753ee8ffb9050d16b2397c84
baddis01 <96215678+baddis01@users.noreply.github.com>
2022-06-08 16:13:26 +0100

    added navbar and filters topics via hardcoded buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
-import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ArticlesList from "./components/ArticlesList";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
+        <NavBar />
         <Routes>
           <Route path="/" element={<ArticlesList />} />
           <Route path="/topics/:topic" element={<ArticlesList />} />
